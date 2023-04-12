eeca9b6a3b46404944b823ed9edce770552bd766
siwenboxue <siwenboxue@qq.com>
2023-04-12 20:21:57 +0800

    Node改造

--- src/main/java/com/quickcode/entity/Node.java
+++ src/main/java/com/quickcode/entity/Node.java
@@ -1,5 +1,8 @@
 package com.quickcode.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 双向链表节点
  */
@@ -7,12 +10,21 @@ public class Node {
     public int val;
     public Node left;
     public Node right;
+    public List<Node> neighbors;
 
     public Node() {
+        val = 0;
+        neighbors = new ArrayList<>();
     }
 
     public Node(int _val) {
         val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
     }
 
     public Node(int _val, Node _left, Node _right) {
